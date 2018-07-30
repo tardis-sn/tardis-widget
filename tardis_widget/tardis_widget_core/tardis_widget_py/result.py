c7e8f1968863be9f89eb7d07dbcf1a82491d48c2
Laud Bentil <laudbentil@gmail.com>
2018-07-30 06:37:07 +0000

    add initial plot code

--- tardis_widget/tardis_widget_core/tardis_widget_py/result.py
+++ tardis_widget/tardis_widget_core/tardis_widget_py/result.py
@@ -1,21 +1,21 @@
 import ipywidgets as widgets
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 from traitlets import Unicode
 from ipywidgets import Layout, Button, Box, VBox
 from IPython.display import display
+from IPython.core.pylabtools import print_figure
+plt.ioff()
 
 @widgets.register
 class Result(widgets.DOMWidget):
     """
     Result Widget for tardis
 
-    It has the following as methods for now:
+    It has the following as methods:
 
-    - Model Parameters
-
-    - Tabulated Results
-
-    - Visualized Results
+    - Plots data into graph
 
     """
     _view_name = Unicode("ResultView").tag(sync=True)
@@ -26,20 +26,13 @@ class Result(widgets.DOMWidget):
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
 
 
-    def model_parameters(self):
-        pass
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
 
-
-    def tabulated_results(self, input_a, input_b):
-        a = range(input_a)
-        b = range(input_b)
-        df = pd.DataFrame()
-        df['Rad. temp'] = a # change to read values for Rad. temp column
-        df['Ws'] = b # change to read values for Ws column
-        df.index = range(1, len(df)+1)
-        df.index.names = ['Shell']
-        return df
-
-
-    def visualized_results(self):
-        pass
+    def _repr_png_(self):
+        fig = plt.figure()
+        plt.plot(self.row, self.column)
+        data = print_figure(fig, 'png')
+        plt.close()
+        return data
